5fad600cd20ac3dc1cb0b9fbb542d05414b96a0a
Maveric4 <maveriok@student.agh.edu.pl>
2020-03-04 17:01:58 +0100

    Mobile app mqtt msg receiving

---
+++ mobile_app/SudokuSolver/SudokuSolver/SudokuSolver/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace SudokuSolver
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ mobile_app/SudokuSolver/SudokuSolver/SudokuSolver/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using System.Net;
+
+namespace SudokuSolver
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        MqttClient client;
+        string mqttMSG = string.Empty;
+        string topic = string.Empty;
+        string MQTT_BROKER_ADDRESS = "192.168.9.201";
+        public MainPage()
+        {
+            InitializeComponent();
+            client = new MqttClient(IPAddress.Parse(MQTT_BROKER_ADDRESS));
+
+            // register to message received 
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+            string clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId);
+            client.Subscribe(new string[] { "sudoku/msg" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+        }
+
+        void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            mqttMSG = System.Text.Encoding.UTF8.GetString(e.Message);
+            topic = e.Topic;
+        }
+
+        void OnShowMsgButtonClicked(object sender, EventArgs e)
+        {
+            LabelTopic.Text = topic;
+            LabelMsg.Text = mqttMSG;
+        }
+    }
+}
